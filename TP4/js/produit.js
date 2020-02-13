cb9ea33fa24349ac563cd723c014d2cac85135ae
TheArKXIV <arkangefr@outlook.com>
2020-02-13 17:45:11 +0100

    Incr and Decr quantity

--- TP4/js/produit.js
+++ TP4/js/produit.js
@@ -1,1 +1,19 @@
 console.log('Hello world :) !');
+
+$(document).ready(function(){
+    $('#incqtt').click(function() {
+        $('#quantity').val( function(i, oldval) {
+            return ++oldval;
+        });
+        $('#quantity').html(parseInt($('#quantity').html(), 10)+1)
+    })
+
+    $('#decqtt').click(function() {        
+        if($('#quantity').val() > 1){
+            $('#quantity').val( function(i, oldval) {
+                return --oldval;
+            });
+            $('#quantity').html(parseInt($('#quantity').html(), 10)-1)
+        }
+    })
+});
